c316dd4ca29c9becf460be6142b94bcfe9942a1e
Rafael Carvalho Rosina <39815593+rafaelcl14@users.noreply.github.com>
2021-05-28 11:46:40 -0400

    Final commit project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ac31af2179ddee660976b7e1cfe5c291'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5b076af9-5163-46b4-83b9-ff5575c86ec4'
+};
